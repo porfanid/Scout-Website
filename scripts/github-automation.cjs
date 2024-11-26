9117362bb4c3c0ba2afa32269a42bfb26100ab59
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-11-27 01:14:34 +0200

    Added scripts to automate some git functions

---
+++ scripts/github-automation.cjs
@@ -0,0 +1,25 @@
+const simpleGit = require('simple-git');
+const { execSync } = require('child_process');
+const git = simpleGit();
+
+async function createTagAndPush() {
+    try {
+        // Generate the changelog
+        execSync('npm run changelog', { stdio: 'inherit' });
+
+        // Get the latest version from package.json
+        const { version } = require('../package.json');
+
+        // Create a new tag
+        await git.addTag(`v${version}`);
+
+        // Push the new tag
+        await git.pushTags('origin');
+
+        console.log(`Tag v${version} created and pushed successfully.`);
+    } catch (error) {
+        console.error('Error creating and pushing tag:', error);
+    }
+}
+
+createTagAndPush();
