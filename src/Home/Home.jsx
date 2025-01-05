924f1d30cc9ce7ed08b841c17a91ee9b0b58a782
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2025-01-05 18:03:20 +0200

    Updated the name in the license and the theme colors

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,12 +5,13 @@ import siteTitleSvg from "./site_title.svg";
 import Departments from "./Departments/Departments.jsx";
 import backgroundImage from "../Navbar/topShadow.png"
 import {useTheme} from "@mui/styles";
-import BecomeScout from "./BecomeScout.jsx";
-
+import BecomeScout from "./BecomeScout/BecomeScout.jsx";
 
 /**
- * Toggles the drawer open or closed.
- * @returns {Function} The function to toggle the drawer.
+ * Home component that renders the main page with a hero section,
+ * a call-to-action section, and a departments section.
+ *
+ * @returns {JSX.Element} The Home component.
  */
 function Home() {
 
@@ -18,82 +19,92 @@ function Home() {
     const mainBgColor = theme.palette.background.default;
 
     return (
-        <>
-            {/* Hero Section */}
-            <Box display="flex" justifyContent="center" sx={{
-                width: "100%",
-                padding: '2rem',
-                position: 'relative',
-                marginBottom:"0px",
-                backgroundImage: `url(${backgroundImage})`, // Add the background image
-                backgroundSize: 'cover', // Ensure the image covers the entire area
-                backgroundPosition: 'center', // Center the image
-                paddingTop:"15vh",
-                '::before': theme.palette.mode==="dark"?{
-                    content: '""',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: `${mainBgColor}80`, // Use the main background color with opacity
-                    zIndex: 0,
-                }:{}
-            }}>
-                <Grid container spacing={0} alignItems="center" justifyContent="center" sx={{ flexDirection: { xs: 'column', md: 'row' } }}>
-                    {/* First Grid item with the SVG image */}
-                    <Grid
-                        xs={12}
-                        md={6}
+            <>
+                {/* Hero Section */}
+                <Box
+                        display="flex"
+                        justifyContent="center"
                         sx={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
+                            width: "100%",
+                            padding: '2rem',
                             position: 'relative',
-                            zIndex: 1,
-                            padding: { xs: '20px', md: '0' }, // Add padding for smaller screens
+                            marginBottom:"0px",
+                            backgroundImage: `url(${backgroundImage})`, // Add the background image
+                            backgroundSize: 'cover', // Ensure the image covers the entire area
+                            backgroundPosition: 'center', // Center the image
+                            paddingTop:"15vh",
+                            '::before': theme.palette.mode==="dark"?{
+                                content: '""',
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: '100%',
+                                backgroundColor: `${mainBgColor}80`, // Use the main background color with opacity
+                                zIndex: 0,
+                            }:{}
                         }}
+                >
+                    <Grid
+                            container
+                            spacing={0}
+                            alignItems="center"
+                            justifyContent="center"
+                            sx={{ flexDirection: { xs: 'column', md: 'row' } }}
                     >
-                        <Box
-                                component="img"
-                                src={siteTitleSvg}
-                                alt="Site Title"
+                        {/* First Grid item with the SVG image */}
+                        <Grid
+                                xs={12}
+                                md={6}
                                 sx={{
-                                    minWidth: { md: '50vw', xs: '80vw' },
-                                    maxWidth: '100vw',
-                                    height: 'auto'
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    position: 'relative',
+                                    zIndex: 1,
+                                    padding: { xs: '20px', md: '0' }, // Add padding for smaller screens
                                 }}
-                        />
+                        >
+                            <Box
+                                    component="img"
+                                    src={siteTitleSvg}
+                                    alt="Site Title"
+                                    sx={{
+                                        minWidth: { md: '50vw', xs: '80vw' },
+                                        maxWidth: '100vw',
+                                        height: 'auto'
+                                    }}
+                            />
+                        </Grid>
+
+                        {/* Second Grid item with the hero image */}
+                        <Grid
+                                xs={12}
+                                md={6}
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    position: 'relative',
+                                    zIndex: 0,
+                                    marginLeft: { xs: 0, md: '-8vw' }  // Adjust this value for desired overlap
+                                }}
+                        >
+                            <img
+                                    src={heroImage}
+                                    alt="Hero Image"
+                                    style={{ width: '100%', height: 'auto', maxWidth: '700px', objectFit: 'contain' }} // Ensure the hero image fits well
+                            />
+                        </Grid>
                     </Grid>
+                </Box>
 
-                    {/* Second Grid item with the hero image */}
-                    <Grid
-                        xs={12}
-                        md={6}
-                        sx={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            position: 'relative',
-                            zIndex: 0,
-                            marginLeft: { xs: 0, md: '-8vw' }  // Adjust this value for desired overlap
-                        }}
-                    >
-                        <img
-                            src={heroImage}
-                            alt="Hero Image"
-                            style={{ width: '100%', height: 'auto', maxWidth: '700px', objectFit: 'contain' }} // Ensure the hero image fits well
-                        />
-                    </Grid>
-                </Grid>
-            </Box>
+                {/* Call-to-Action Section */}
+                <BecomeScout/>
 
-            <BecomeScout/>
-
-            {/* Departments Section */}
-
-            <Departments/>
-        </>
+                {/* Departments Section */}
+                <Departments/>
+            </>
     );
 }
 
